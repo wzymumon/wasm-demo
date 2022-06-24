c5641eedfa7dd3543cfefc81a15489aa1d3a38d0
Ziyu Wang <wzyubill1104@gmail.com>
2022-06-25 01:49:09 +0800

    add wazero demo

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/bytecodealliance/wasmtime-go v0.37.0
 	github.com/wasmerio/wasmer-go v1.0.4
 )
+
+require github.com/tetratelabs/wazero v0.0.0-20220624054950-1489a9f19f39
---
+++ wazero/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "context"
+    "fmt"
+	"github.com/tetratelabs/wazero"
+	"io/ioutil"
+	"time"
+)
+
+var addWasm []byte
+
+// main implements a basic function in both Go and WebAssembly.
+func main() {
+	start := time.Now() // 获取当前时间
+
+	// Choose the context to use for function calls.
+	ctx := context.Background()
+
+    // Read a WebAssembly binary containing an exported "sum" function.
+    addWasm, err := ioutil.ReadFile("demo/simple.wasm")
+    check(err)
+
+	// Create a new WebAssembly Runtime.
+	r := wazero.NewRuntime()
+	defer r.Close(ctx) // This closes everything this Runtime created.
+	
+    // Add a module to the runtime named "wasm/math" which exports one function "add", implemented in WebAssembly.
+	module, err := r.InstantiateModuleFromBinary(ctx, addWasm)
+	check(err)
+
+    // exported "sum" function.
+    sum := module.ExportedFunction("sum")
+
+    // get result
+	result, err := sum.Call(ctx, 5, 37)
+	check(err)
+
+    // wazero Call only return []int64
+	fmt.Printf("sum(5, 37) = %d\n", result[0])
+
+	elapsed := time.Since(start)
+	fmt.Println("wazero-go 执行耗时：", elapsed)
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
